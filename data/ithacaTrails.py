8755949e6e6a36448d35e98107d409031b374bdf
Jacqueline Wen <jw2347@cornell.edu>
2022-10-15 10:45:03 -0400

    added difficulty and distance

--- data/ithacaTrails.py
+++ data/ithacaTrails.py
@@ -1,5 +1,6 @@
 import glob
 import psycopg2
+import math
 import pandas as pd
 
 files = glob.glob('./*.csv')
@@ -19,8 +20,7 @@ cursor = conn.cursor()
 for i in files:
     data = pd.read_csv(i)
 
-    cursor.execute(f"""INSERT INTO "Trail" VALUES (gen_random_uuid(), (SELECT id from "dm_College" where name = 'Cornell University'), '{i.split('/')[-1].split('.')[0]}')  """)
-    conn.commit()
+    
     position = []
     longitude = []
     latitude = []
@@ -33,6 +33,40 @@ for i in files:
         longitude.append(row['Longitude'])
         latitude.append(row['Latitude'])
         elevation.append(row['Elevation'])
+
+    dist = 0
+    prevlong = longitude[0]
+    prevlat = latitude[0]
+    prevelev = elevation[0]
+    maxelevation = elevation[0]
+    minelevation = elevation[0]
+
+    for j in range(len(longitude)):
+        if j == 0:
+            continue
+        maxelevation = max(maxelevation, elevation[j])
+        minelevation = min(minelevation, elevation[j])
+        newdist = math.sqrt(pow(111000*(longitude[j]-prevlong), 2) + pow(111000*(latitude[j]-prevlat), 2) + pow(elevation[j]-prevelev, 2))
+        prevlong = longitude[j]
+        prevlat = latitude[j]
+        prevelev = elevation[j]
+        dist += newdist
+    
+    deltaele = maxelevation - minelevation
+
+    counter = 1
+    if deltaele > 200:
+        counter = counter + 1
+    
+    if (dist > 8000):
+        counter = counter + 1
+    
+    dist = dist*6.213712e-4
+    deltaele = deltaele * 3.28083993602
+
+    cursor.execute(f"""INSERT INTO "Trail" ("id", "name", "collegeid", "image", "distance", "elevationChange", "difficulty") VALUES (gen_random_uuid(), '{i.split('/')[-1].split('.')[0]}', (SELECT id from "dm_College" where name = 'Cornell University'), null, {round(dist, 2)}, {round(deltaele, 2)}, {counter})  """)
+    conn.commit()
+
 
 
     for a, b, c, d in zip(position, longitude, latitude, elevation):
