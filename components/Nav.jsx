c958629df21452e6c38e758cb7b2063baa91dde4
mikeyliang <liangmichael27822@gmail.com>
2022-10-15 17:11:12 -0400

    Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,13 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
+
+import HomeIcon from '../assets/icons/home.svg'
+import MapIcon from '../assets/icons/map-pin.svg'
+import UserIcon from '../assets/icons/user-circle.svg'
 
 import Logo from '../assets/icons/logo.svg'
 
-export default function Nav() {
+import { useFonts } from 'expo-font';
+
+export default function Nav(props) {
+
     
     return (
-        <View className="absolute">
-            <Text>Hiii</Text>
-            <Logo width={120} height={120} />
+        <View className="absolute bottom-0 bg-white w-full pb-6 pt-4 rounded-full">
+            <View className="flex flex-row justify-between px-12 items-center">
+                <TouchableOpacity className={`flex flex-col justify-center items-center`} style={{}}>
+                    <HomeIcon width={25} height={25} style={{color: props.section === 'Home' ? '#16a34a': '#6b7280'}}/>
+                    <Text className={`font-[Inter-Bold] text-xs text-gray-500 ${props.section === 'Home' ? 'text-green-600' : ''}`}>Home</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity className={`flex flex-col justify-center items-center`}>
+                    <MapIcon width={25} height={25} style={{color: props.section === 'Map' ? '#16a34a': '#6b7280'}}/>
+                    <Text className="font-[Inter-Bold] text-xs text-gray-500">Map</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity className={`flex flex-col justify-center items-center`}>
+                    <UserIcon width={25} height={25} style={{color: props.section === 'Profile' ? '#16a34a': '#6b7280'}}/>
+                    <Text className="font-[Inter-Bold] text-xs text-gray-500">Profile</Text>
+                </TouchableOpacity>
+
+            
+            </View>
+            
         </View>
     )
 }
