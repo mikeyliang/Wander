636ea021aedbdd3f58532802d28767cff9b1e62d
byl23 <brandonyusenli@gmail.com>
2022-10-15 13:27:02 -0400

    nav stuff

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,14 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-
-import SVG from '../assets/icons/logo.svg';
+import Logo from '../assets/icons/logo.svg'
 
 export default function Nav() {
     
     return (
         <View className="absolute">
             <Text>Hiii</Text>
-            <SVG />
+            <Logo width={120} height={120} />
         </View>
     )
 }
