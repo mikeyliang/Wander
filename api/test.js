6200e1725fa0ad06928dcb1ff71bd4ba004eb6f9
Jacqueline Wen <jw2347@cornell.edu>
2022-10-15 12:21:00 -0400

    trails api

--- api/test.js
+++ api/test.js
@@ -1,5 +1,12 @@
 import prisma from "./lib/prisma.js"
 
-const trails = await prisma.trail.findMany()
+const trails = await prisma.trail.findMany({
+    where: {
+        collegeid: "ef8f0795-9036-4301-b56f-48995494fcfe"
+    },
+    include: {
+        trailposition: true
+    }
+})
 
 console.log(trails)
